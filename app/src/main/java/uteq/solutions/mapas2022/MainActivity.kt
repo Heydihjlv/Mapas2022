49f4ab3a1bd7d689903c610ca3dc6fdce67e8879
Heydihjlv <107517086+Heydihjlv@users.noreply.github.com>
2022-08-19 17:43:20 -0500

    Initial commit

---
+++ app/src/main/java/uteq/solutions/mapas2022/MainActivity.kt
@@ -0,0 +1,75 @@
+package uteq.solutions.mapas2022
+
+import android.graphics.Point
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback,GoogleMap.OnMapClickListener {
+    lateinit var mMap:GoogleMap
+    var puntos:ArrayList<LatLng> = ArrayList<LatLng>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mapFragment: SupportMapFragment = getSupportFragmentManager()
+            .findFragmentById(R.id.map) as SupportMapFragment
+
+        mapFragment.getMapAsync(this);
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap;
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        val camUpd1: CameraUpdate =
+            CameraUpdateFactory.newLatLngZoom(
+                LatLng(-1.0799, -79.50133), 25F);
+
+        mMap.moveCamera(camUpd1);
+
+        mMap.setOnMapClickListener(this)
+    }
+
+    override fun onMapClick(point: LatLng) {
+        //val proj:Projection = mMap.getProjection();
+       // val coord:Point = proj.toScreenLocation(point);
+       // Toast.makeText(
+       // this.applicationContext,
+       // "Click\n" +
+       // "Lat: " + point.latitude + "\n" +
+      //  "Lng: " + point.longitude + "\n" +
+      //  "X: " + coord.x + " - Y: " + coord.y,
+     //   Toast.LENGTH_SHORT).show();
+
+
+        puntos.add(point)
+        mMap.addMarker(
+            MarkerOptions().position(point)
+                .title("lugar"+puntos.size)
+        )
+
+        mMap.setOnMarkerClickListener()
+        if(puntos.size==4){
+        var lineas: PolylineOptions= PolylineOptions()
+
+        for (punto:LatLng in puntos)
+            lineas.add(punto)
+        lineas.add(puntos.get(0))
+        lineas.width(8F)
+
+        mMap.addPolyline(lineas)
+
+        puntos.clear()
+
+        }
+
+    }
+}
